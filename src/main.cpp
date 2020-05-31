ac45ab4213a329d2c627cf2fdfb3f5bdd96a0f7a
Anton Karmanov <bergertroll@insiberia.net>
2020-05-31 22:09:35 +0500

    Update main()

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,25 @@
 
 #include "matrix.h"
 
+void pretty_print(otus::Matrix<int, 0> &m) {
+  for (int i { 1 }; i < 9; ++i) {
+    for (int j { 1 }; j < 9; ++j) {
+      std::cout << m[i][j] << ' ';
+    }
+    std::cout << std::endl;
+  }
+}
+
 int main() {
-  otus::Matrix<int, -1> m { };
-  std::cout << m.size() << std::endl;
-  m[100][100] = 108;
-  std::cout << m.size() << std::endl;
-  std::cout << m[0][0] << std::endl;
-  std::cout << m[100][100] << std::endl;
+  otus::Matrix<int, 0> m { };
+
+  for (int i { }; i < 10; ++i) {
+    m[i][i] = i;
+    m[i][9 - i] = 9 - i;
+  }
+
+  pretty_print(m);
+
+  std::cout << std::endl << "Occupied cells num: " << m.size() << std::endl;
+
 }
