6e47842437e39c389ce3d9274314bc4e7927e4f2
Anton Karmanov <bergertroll@insiberia.net>
2020-05-31 17:31:34 +0500

    Implement Vector

--- inc/matrix.h
+++ inc/matrix.h
@@ -1,27 +1,76 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
-#include <iostream>
+#include <cstddef>
 #include <unordered_map>
 
 namespace otus {
+
   template <typename T, T default_val>
   class Vector {
+    class Pool {
+    public:
+      void assign(size_t index, T value) {
+        if (value != default_val) {
+          data[index] = value;
+        } else {
+          if (data.count(index)) data.erase(index);
+        }
+      }
+
+      T get(size_t index) {
+        if (data.count(index)) return data[index];
+        else return default_val;
+      }
+
+      size_t size() { return data.size(); }
+
+    private:
+      std::unordered_map<size_t, T> data { };
+    };
+
+    class Proxy {
+    public:
+      Proxy(size_t index, Pool &pool): index(index), pool(pool) { }
+
+      ~Proxy() = default;
+      Proxy(Proxy const &other) = delete;
+      Proxy* operator =(Proxy const &other) = delete;
+      Proxy(Proxy &&other) = delete;
+      Proxy* operator =(Proxy &&other) = delete;
+
+      Proxy* operator =(T const &value) {
+        pool.assign(index, value);
+        return this;
+      }
+
+      operator T() { return pool.get(index); }
+
+    private:
+      size_t index;
+      Pool &pool;
+    };
+
   public:
     Vector() { }
-    size_t size() { }
-    T& operator[](size_t index) { }
+    size_t size() { return pool.size(); }
+    Proxy operator [](size_t index) { return Proxy(index, pool); }
+
   private:
-    std::unordered_map<size_t, T> data { };
+    Pool pool;
   };
 
   template <typename T, T default_val>
   class Matrix {
   public:
     Matrix() {}
-    size_t size() {}
-    Vector<T, default_val>& operator[](size_t index) {}
+
+    size_t size() { return vector.size(); }
+
+    Vector<T, default_val>& operator [](size_t index) { }
+
   private:
+    Vector<T, default_val> vector;
   };
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
 
+#include "matrix.h"
+
 int main() {
+  otus::Vector<int, -1> v { };
+  std::cout << v.size() << std::endl;
+  v[100] = 108;
+  std::cout << v.size() << std::endl;
+  std::cout << v[0] << std::endl;
+  std::cout << v[100] << std::endl;
 }
